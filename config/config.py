451ca942353202c80d21d0177a1e79434ca771b0
islamalla <72531862+FearlessDK09@users.noreply.github.com>
2024-06-24 16:48:15 +0300

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,6 @@
+# Configuration file for storing API keys and other constants
+
+BINANCE_API_KEY = "your_binance_api_key"
+BINANCE_API_SECRET = "your_binance_api_secret"
+TELEGRAM_TOKEN = 'your_telegram_token'
+TELEGRAM_CHAT_IDS = ['your_telegram_chat_id']
